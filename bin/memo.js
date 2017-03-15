bcd9f43fc4e575c898fda3f827f3c1090295ee53
FujiHaruka <fujiharuka@realglobe.jp>
2017-03-16 00:47:32 +0900

    Basic functions

---
+++ bin/memo.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+const commander = require('commander')
+const memo = require('../lib')
+
+commander
+  .version(require('../package.json').version)
+  .alias('v')
+
+let actions = [
+  ['repos', memo.setRepository],
+  ['list [filter]', memo.list],
+  ['add', memo.add],
+  ['rm [id]', memo.remove],
+  ['show [id]', memo.show],
+  ['edit [id]', memo.edit],
+  ['drop', memo.drop]
+]
+
+for (let action of actions) {
+  let [name, func] = action
+  commander.command(name).action(func)
+}
+
+commander.parse(process.argv)
